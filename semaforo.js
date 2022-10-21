9b95c0e7b7394331a25afbe438c3c83ab64eddce
Thainno Santana <thainnosv@gmail.com>
2022-10-20 22:29:37 -0300

    Finalização do JS

--- semaforo.js
+++ semaforo.js
@@ -1,14 +1,33 @@
 const img = document.querySelector("#img")
 const buttons = document.querySelector("#buttons")
+let colorIndex = 0
+let intervalId = null
 
 const trafficLight = (event) => {
-    turnOn[event.target.id]()
+    stopAutomatic();
+    turnOn[event.target.id]()    
+}
+
+const nextIndex = () => {
+    colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+}
+
+const changeColor = () => {
+    const colors = ["red", "yellow", "green"];
+    const color = colors[colorIndex];
+    turnOn[color]();
+    nextIndex()
+}
+
+const stopAutomatic = () => {
+    clearInterval(intervalId)
 }
 
 const turnOn = {
     'red':      () => img.src = "./imagens/vermelho.png",
     'yellow':   () => img.src = "./imagens/amarelo.png",
-    'green':    () => img.src = "./imagens/verde.png"
+    'green':    () => img.src = "./imagens/verde.png",
+    'automatic':() => intervalId = setInterval(changeColor, 1000)
 }
 
 buttons.addEventListener("click", trafficLight)
